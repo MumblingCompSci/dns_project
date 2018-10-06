dcdf920ab8ccd31d49611246624d17b19e3cc78b
Garrett Quintero <garrett.quintero@gmail.com>
2018-10-05 22:27:01 -0600

    coping 'Handle Inputs' code from echo_s

--- dug.cpp
+++ dug.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+
+
 int main (int argc, char *argv[]) {
     struct Header {
         unsigned short id;
@@ -25,35 +27,51 @@ int main (int argc, char *argv[]) {
         unsigned auth_count :16; // authority entries
         unsigned add_count :16; // resource entries
 
+    };
+
+// ********************************************************************
+    // * Process the command line arguments
+    // ********************************************************************
+    boost::log::add_console_log(std::cout, boost::log::keywords::format = "%Message%");
+    boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::info);
+
+    // ********************************************************************
+    // * Process the command line arguments
+    // ********************************************************************
+    int opt = 0;
+    while ((opt = getopt(argc,argv,"v")) != -1) {
+
+        switch (opt) {
+            case 'v':
+                boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::debug);
+                break;
+            case ':':
+            case '?':
+            default:
+                std::cout << "useage: " << argv[0] << " -v" << std::endl;
+                exit(-1);
+        }
     }
+// ******************************************************************
 
     // Step One: create the socket
     int sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+    DEBUG << "Calling socket() -> " << sockfd << ENDL;
 
     // Step Two: fill in the address
     struct sockaddr_in servaddr;
     bzero(&servaddr, sizeof(servaddr));
-    if (!inet_aton("138.67.1.2", $servaddr.sin_addr)) {
+    if (!inet_aton("138.67.1.2", &servaddr.sin_addr)) {
         FATAL << "inet_aton failed" << ENDL;
         exit(-1);
     }
 
     // Step Three: call connect
     if (connect(sockfd, (const sockaddr*) &servaddr, sizeof(servaddr))) {
-        FATAL << "Connect failed." < ENDL;
+        FATAL << "Connect failed." << ENDL;
         FATAL << strerror(errno) << ENDL;
     }
 
-    // Step Four: read() or write()
-    if((bytesRead = read(sockfd, message, 5000)) < 0) {
-        FATAL << "read returned error" << sterror(errno) << ENDL;
-        exit(-1);
-    };
-
-    if ((bytesSent = write(sockfd, message, 5000)) < 0) {
-        FATAL << "read returned error" << strerror(errno) << ENDL;
-        exit(-1);
-    };
-
+    // Step Four: write and read as needed
     return 0;
 }
