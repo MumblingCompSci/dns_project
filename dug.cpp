31ba0ded1aa6ffc02425df6201286674a07c6cb1
Garrett Quintero <garrett.quintero@gmail.com>
2018-10-05 21:56:10 -0600

    made CLION fix my indentation stuff

--- dug.cpp
+++ dug.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
     struct PACKET {
-      unsigned short id;
+        unsigned short id;
 
 // the good header stuff
-unsigned char qr :1; // query bit
-unsigned char opcode :4; // opcode
-unsigned char aa :1; // authoritative answer
-unsigned char tc :1; // truncate
-unsigned char rd :1; // recursion desired
-unsigned char ra :1; // recursion available
-unsigned char z  :4; // set to 0
-unsigned char rcode :4; // response code
+        unsigned char qr :1; // query bit
+        unsigned char opcode :4; // opcode
+        unsigned char aa :1; // authoritative answer
+        unsigned char tc :1; // truncate
+        unsigned char rd :1; // recursion desired
+        unsigned char ra :1; // recursion available
+        unsigned char z  :4; // set to 0
+        unsigned char rcode :4; // response code
 
 // pretty much the body of the packet
-unsigned q_count :16; // question entries
-unsigned ans_count :16; // answer entries
-unsigned auth_count :16; // authority entries
-unsigned add_count :16; // resource entries
+        unsigned q_count :16; // question entries
+        unsigned ans_count :16; // answer entries
+        unsigned auth_count :16; // authority entries
+        unsigned add_count :16; // resource entries
 
     }
 
@@ -34,25 +34,25 @@ unsigned add_count :16; // resource entries
     struct sockaddr_in servaddr;
     bzero(&servaddr, sizeof(servaddr));
     if (!inet_aton("138.67.1.2", $servaddr.sin_addr)) {
-      FATAL << "inet_aton failed" << ENDL;
-      exit(-1);
+        FATAL << "inet_aton failed" << ENDL;
+        exit(-1);
     }
 
     // Step Three: call connect
     if (connect(sockfd, (const sockaddr*) &servaddr, sizeof(servaddr))) {
-      FATAL << "Connect failed." < ENDL;
-      FATAL << strerror(errno) << ENDL;
+        FATAL << "Connect failed." < ENDL;
+        FATAL << strerror(errno) << ENDL;
     }
 
     // Step Four: read() or write()
     if((bytesRead = read(sockfd, message, 5000)) < 0) {
-      FATAL << "read returned error" << sterror(errno) << ENDL;
-      exit(-1);
+        FATAL << "read returned error" << sterror(errno) << ENDL;
+        exit(-1);
     };
 
     if ((bytesSent = write(sockfd, message, 5000)) < 0) {
-      FATAL << "read returned error" << strerror(errno) << ENDL;
-      exit(-1);
+        FATAL << "read returned error" << strerror(errno) << ENDL;
+        exit(-1);
     };
 
     return 0;
