491d78aa160e15175bfc5f8247b05fbeb9cf3371
Garrett Quintero <garrett.quintero@gmail.com>
2018-10-09 02:08:36 -0600

    stuff

--- dug.cpp
+++ dug.cpp
@@ -18,7 +18,7 @@ int main (int argc, char *argv[]) {
     processHostname(name);
     cout << "Converted Message : " << name << endl;
 
-    struct {
+    struct Header{
         uint16_t id;
 
         struct {
@@ -37,80 +37,80 @@ int main (int argc, char *argv[]) {
         uint16_t ans_count;
         uint16_t auth_count;
         uint16_t add_count; 
-    } header;
+    };
 
-    struct {
+    struct Q_details{
         uint16_t qtype;
         uint16_t qclass;
-    } q_details;
+    };
 
 
 
-    // Step One : Create the socket //
+    // Step One: create the socket
     int sockfd = socket(PF_INET, SOCK_DGRAM, 0);
-    cout << "Called socket . sockfd : " << sockfd << endl;
+    DEBUG << "Calling socket() -> " << sockfd << ENDL;
 
-    // Step Two : server stuff //
+    // Step Two: fill in the address
     struct sockaddr_in servaddr;
     bzero(&servaddr, sizeof(servaddr));
-
     servaddr.sin_family = PF_INET;
     servaddr.sin_port = htons(53);
 
-    if (!inet_aton(dns_serv, &servaddr.sin_addr)) {
-        cerr << "inet_aton failed" << endl;
-        cout << "Closing Socket" << endl;
-        close(sockfd);
+    if (!inet_aton(argv[2], &servaddr.sin_addr)) {
+        FATAL << "inet_aton failed" << ENDL;
         exit(-1);
     }
-    // Step Three : connect the socket //
-    cout << "Connecting socket..." << endl;
-    int test_int = -1281212;
-    if ((test_int = connect(sockfd, (const sockaddr*) &servaddr, sizeof(servaddr))) < 0) {
-        cerr << "Connect failed " << endl;
-        cerr << strerror(errno) << endl;
-        close(sockfd);
-        exit(-1);
-    }
-    cout << "TEST INT : " << test_int << endl;
+
+    // Step Three: call connect
+    // if (connect(sockfd, (const sockaddr*) &servaddr, sizeof(servaddr))) {
+    //     FATAL << "Connect failed." << ENDL;
+    //     FATAL << strerror(errno) << ENDL;
+    // }
 
     // Step Four : read and write
-
     // write up the packet
-    header.id = htons(getpid());
-    header.flags.rd = htons(0);
-    header.flags.tc = htons(0);
-    header.flags.aa = htons(0);
-    header.flags.opcode = htons(0);
-    header.flags.qr = htons(0);
-    header.flags.rcode = htons(0);
-    header.flags.z = htons(0);
-    header.flags.ra = htons(0);
-    header.q_count = htons(1);
-    header.ans_count = htons(0);
-    header.auth_count = htons(0);
-    header.add_count = htons(0);
+    Header *header = (Header *) calloc(1, sizeof(struct Header));
+    header->id = htons(getpid());
+    header->flags.rd = htons(0);
+    header->flags.tc = htons(0);
+    header->flags.aa = htons(0);
+    header->flags.opcode = htons(0);
+    header->flags.qr = htons(0);
+    header->flags.rcode = htons(0);
+    header->flags.z = htons(0);
+    header->flags.ra = htons(0);
+    header->q_count = htons(1);
+    header->ans_count = htons(0);
+    header->auth_count = htons(0);
+    header->add_count = htons(0);
 
-    q_details.qtype = htons(1);
-    q_details.qclass = htons(1);
+    Q_details * q_details = (Q_details *) calloc(1, sizeof(Q_details));
+    q_details->qtype = htons(1);
+    q_details->qclass = htons(1);
 
     //write stuff to the buffer
     char * write_buff = (char *) calloc(1024, sizeof(char));
     int total_size = 0;
-    memcpy(write_buff, &header, sizeof(header));
-    total_size += sizeof(header);
+    memcpy(write_buff, header, sizeof(struct Header));
+    total_size += sizeof(struct Header);
     memcpy(write_buff + total_size, name, sizeof(name));
     total_size += sizeof(name);
-    memcpy(write_buff + total_size, &q_details, sizeof(q_details));
-    total_size += sizeof(q_details);
+    memcpy(write_buff + total_size, q_details, sizeof(struct Q_details));
+    total_size += sizeof(struct Q_details);
 
     cout << "Sort of expected size : " << total_size << endl;
     char * read_buff = (char *) calloc(1024, sizeof(char));
 
     // Send (write) the query
     int bytesSent = -1;
-    if ((bytesSent = write(sockfd, write_buff, total_size)) < 0) {
-        cerr << "write failed" << endl;
+    // if ((bytesSent = write(sockfd, write_buff, total_size)) < 0) {
+    //     cerr << "write failed" << endl;
+    //     cerr << strerror(errno) << endl;
+    //     close(sockfd);
+    //     exit(-1);
+    // }
+    if ((bytesSent = sendto(sockfd, write_buff, total_size, 0,(const sockaddr*) &servaddr, sizeof(servaddr))) < 0) {
+        cerr << "sendto failed" << endl;
         cerr << strerror(errno) << endl;
         close(sockfd);
         exit(-1);
@@ -119,7 +119,7 @@ int main (int argc, char *argv[]) {
 
     //read the result
     int bytesRead = -1;
-    if ((bytesRead = read(sockfd, read_buff, total_size)) < 0) {
+    if ((bytesRead = read(sockfd, read_buff, 5000)) < 0) {
         cerr << "read failed" << endl;
         cerr << strerror(errno) << endl;
         close(sockfd);
